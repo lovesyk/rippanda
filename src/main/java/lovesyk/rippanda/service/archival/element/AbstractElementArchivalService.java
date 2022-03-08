1fd3ef21656d50ff79b464133a4f0d37fc9f3867
lovesyk <lovesyk@users.noreply.github.com>
2022-03-08 20:37:54 +0100

    Improved filename selection.

--- src/main/java/lovesyk/rippanda/service/archival/element/AbstractElementArchivalService.java
+++ src/main/java/lovesyk/rippanda/service/archival/element/AbstractElementArchivalService.java
@@ -3,10 +3,17 @@ package lovesyk.rippanda.service.archival.element;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,20 +48,51 @@ abstract class AbstractElementArchivalService {
     private static final String UNAVAILABLE_FILENAME = "unavailable.txt";
 
     /**
-     * 259 characters according to Windows MAX_PATH.
+     * 258 characters according to Windows MAX_PATH for files inside drive root.
      */
-    private static final int MAX_PATH_LENGTH = 259;
+    private static final int MAX_PATH_LENGTH = 258;
+
     /**
-     * 127 characters to cover file systems which allow a maximum filename length of
-     * 255 UTF-16 bytes. Subtracting 4 characters to accommodate 3-letter temporary
-     * filename extensions.
+     * Example suffix of maximum length for temporary extensions.
      */
-    private static final int MAX_FILENAME_LENGTH_UNIQUE = 123;
+    private static final String FILENAME_SUFFIX_EXAMPLE_TMP = ".tmp";
     /**
-     * Non-unique filenames need an additional 4 characters to accommodate for
-     * suffixes of (2) up to (99).
+     * Example suffix of maximum length for duplicate filenames.
      */
-    private static final int MAX_FILENAME_LENGTH = MAX_FILENAME_LENGTH_UNIQUE - 5;
+    private static final String FILENAME_SUFFIX_EXAMPLE_DUPLICATE = " (99)";
+
+    /**
+     * Account for file systems which allow up to 255 character file names.
+     */
+    private static final int MAX_FILENAME_LENGTH = 255;
+    /**
+     * Account for the temporary ".tmp" file extension.
+     */
+    private static final int MAX_FILENAME_TMP_OVERHEAD_LENGTH = 4;
+    /**
+     * The maximum filename size in bytes for unique filenames on UTF8 file systems.
+     */
+    private static final int MAX_FILENAME_UNIQUE_UTF8_BYTES = MAX_FILENAME_LENGTH - MAX_FILENAME_TMP_OVERHEAD_LENGTH;
+    /**
+     * The maximum filename size in bytes for unique filenames on UTF16 file
+     * systems.
+     */
+    private static final int MAX_FILENAME_UNIQUE_UTF16_BYTES = MAX_FILENAME_LENGTH - MAX_FILENAME_TMP_OVERHEAD_LENGTH * 2;
+    /**
+     * Account for non-unique suffixes up to 99 -> " (99)"
+     */
+    private static final int MAX_FILENAME_NON_UNIQUE_OVERHEAD_LENGTH = 5;
+    /**
+     * The maximum filename size in bytes for non-unique filenames on UTF8 file
+     * systems.
+     */
+    private static final int MAX_FILENAME_NON_UNIQUE_UTF8_BYTES = MAX_FILENAME_UNIQUE_UTF8_BYTES - MAX_FILENAME_NON_UNIQUE_OVERHEAD_LENGTH;
+    /**
+     * The maximum filename size in bytes for non-unique filenames on UTF16 file
+     * systems.
+     */
+    private static final int MAX_FILENAME_NON_UNIQUE_UTF16_BYTES = MAX_FILENAME_UNIQUE_UTF16_BYTES - MAX_FILENAME_NON_UNIQUE_OVERHEAD_LENGTH * 2;
+
     /**
      * The regular expression to apply for cleanup of filenames during sanitization.
      */
@@ -182,42 +220,127 @@ abstract class AbstractElementArchivalService {
 
         String sanitizedFilename = filename;
 
+        String legalizedFilename = legalizeFilename(sanitizedFilename);
+        if (!legalizedFilename.equals(sanitizedFilename)) {
+            sanitizedFilename = legalizedFilename;
+            LOGGER.debug("Legalized filename to \"{}\".", sanitizedFilename);
+        }
+
+        String truncatedFilename = truncateFilename(dir, sanitizedFilename, unique);
+        if (!truncatedFilename.equals(sanitizedFilename)) {
+            sanitizedFilename = truncatedFilename;
+            LOGGER.debug("Truncated filename to \"{}\".", sanitizedFilename);
+        }
+
+        String nonCollidingFilename = decollideFilename(dir, sanitizedFilename, unique);
+        if (!nonCollidingFilename.equals(sanitizedFilename)) {
+            sanitizedFilename = nonCollidingFilename;
+            LOGGER.debug("Removed filename collisions to \"{}\".", sanitizedFilename);
+        }
+
+        return sanitizedFilename;
+    }
+
+    /**
+     * Legalizes the filename by ensuring it contains no illegal characters.
+     * 
+     * @param filename the filename to legalize, not <code>null</code>
+     * @return the legalized filename, never <code>null</code>
+     */
+    private String legalizeFilename(String filename) {
         for (Entry<Character, Character> entry : illegalCharMapping.entrySet()) {
             Character key = entry.getKey();
             Character value = entry.getValue();
 
-            sanitizedFilename = sanitizedFilename.replace(key, value);
+            filename = filename.replace(key, value);
         }
-        sanitizedFilename = sanitizedFilename.replaceAll(FILENAME_CLEANUP_PATTERN, StringUtils.EMPTY);
+        return filename.replaceAll(FILENAME_CLEANUP_PATTERN, StringUtils.EMPTY);
+    }
 
-        if (LOGGER.isDebugEnabled() && !sanitizedFilename.equals(filename)) {
-            LOGGER.debug("Removed potentially unsafe characters resulting in \"{}\".", sanitizedFilename);
-        }
+    /**
+     * Truncates the filename by ensuring path and filename length do not exceed
+     * file system limits.
+     * 
+     * @param dir      the directory the file be later saved to, not
+     *                 <code>null</code>
+     * @param filename the filename to truncate, not <code>null</code>
+     * @param unique   whether the filename is unique or a (1), (2) etc. should be
+     *                 appended
+     * @return the truncated filename, never <code>null</code>
+     * @throws RipPandaException on failure
+     */
+    private String truncateFilename(Path dir, String filename, boolean unique) throws RipPandaException {
+        String baseName = FilenameUtils.getBaseName(filename);
+        String dottedExtension = filename.substring(baseName.length());
 
-        Path file = dir.resolve(sanitizedFilename).toAbsolutePath();
-        int maxPathLengthDiff = file.toString().length() - MAX_PATH_LENGTH;
-        int maxFilenameLength = unique ? MAX_FILENAME_LENGTH_UNIQUE : MAX_FILENAME_LENGTH;
-        int maxFilenameLengthDiff = sanitizedFilename.length() - maxFilenameLength;
-        int excessiveLength = Math.max(maxPathLengthDiff, maxFilenameLengthDiff);
+        String maxSuffix = (unique ? StringUtils.EMPTY : FILENAME_SUFFIX_EXAMPLE_DUPLICATE) + dottedExtension + FILENAME_SUFFIX_EXAMPLE_TMP;
 
-        String baseName = FilenameUtils.getBaseName(sanitizedFilename);
-        String dottedExtension = sanitizedFilename.substring(baseName.length());
-
-        if (excessiveLength > 0) {
-            LOGGER.debug("Filename needs to be shortened by {} characters.", excessiveLength);
-
-            if (excessiveLength > baseName.length()) {
-                throw new RipPandaException("Cannot shorten file name enough to fulfil limits.");
+        Path file = dir.resolve(baseName + maxSuffix).toAbsolutePath();
+        int excessivePathLength = file.toString().length() - MAX_PATH_LENGTH;
+        String truncatedBaseNameForPath = baseName;
+        if (excessivePathLength > 0) {
+            if (excessivePathLength >= baseName.length()) {
+                throw new RipPandaException("Cannot truncate file name enough to fulfil limits.");
             }
-
-            String baseNameCut = baseName.substring(0, baseName.length() - excessiveLength).trim();
-            sanitizedFilename = baseNameCut + dottedExtension;
-
-            LOGGER.debug("Filename was shortened to \"{}\".", sanitizedFilename);
+            truncatedBaseNameForPath = StringUtils.substring(baseName, 0, -excessivePathLength);
         }
 
-        baseName = FilenameUtils.getBaseName(sanitizedFilename);
-        String nonCollidingFilename = sanitizedFilename;
+        int maxUtf8ByteCount = unique ? MAX_FILENAME_UNIQUE_UTF8_BYTES : MAX_FILENAME_NON_UNIQUE_UTF8_BYTES;
+        String truncatedBaseNameUtf8 = truncateBaseName(baseName, maxSuffix, maxUtf8ByteCount, StandardCharsets.UTF_8);
+
+        int maxUtf16ByteCount = unique ? MAX_FILENAME_UNIQUE_UTF16_BYTES : MAX_FILENAME_NON_UNIQUE_UTF16_BYTES;
+        String truncatedBaseNameUtf16 = truncateBaseName(baseName, maxSuffix, maxUtf16ByteCount, StandardCharsets.UTF_16);
+
+        String minBaseName = Arrays.asList(truncatedBaseNameForPath, truncatedBaseNameUtf8, truncatedBaseNameUtf16).stream()
+                .min(Comparator.comparingInt(String::length)).get();
+        String strippedFilename = StringUtils.strip(minBaseName + dottedExtension);
+
+        return strippedFilename;
+    }
+
+    /**
+     * Truncates the base name by ensuring it does not exceed a certain byte count
+     * while considering possible suffixes.
+     * 
+     * @param baseName  the base name to truncate, not <code>null</code>
+     * @param suffix    a possible suffix to consider for truncation, not
+     *                  <code>null</code>
+     * @param byteCount the maximum byte count the whole filename should fit in
+     * @param charset   the character set used for encoding, not <code>null</code>
+     * @return the truncated base name, never <code>null</code>
+     * @throws RipPandaException on failure
+     */
+    private String truncateBaseName(String baseName, String suffix, int byteCount, Charset charset) throws RipPandaException {
+        int baseNameByteCount = byteCount - suffix.getBytes(charset).length;
+        if (baseNameByteCount <= 0) {
+            throw new RipPandaException("Cannot truncate file name enough to fulfil limits.");
+        }
+        byte[] outputBytes = new byte[baseNameByteCount];
+        CharBuffer inBuffer = CharBuffer.wrap(baseName.toCharArray());
+        ByteBuffer outBuffer = ByteBuffer.wrap(outputBytes);
+
+        CharsetEncoder encoder = charset.newEncoder();
+        encoder.encode(inBuffer, outBuffer, true);
+
+        return new String(outputBytes, 0, outBuffer.position(), charset);
+    }
+
+    /**
+     * Removes possible collisions from the filename by appending a numerical suffix
+     * in case it already exists or removing it if it should be unique.
+     * 
+     * @param dir      the directory the file be later saved to, not
+     *                 <code>null</code>
+     * @param filename the filename to truncate, not <code>null</code>
+     * @param unique   whether the filename is unique or a (1), (2) etc. should be
+     *                 appended
+     * @return the collision-free filename, never <code>null</code>
+     * @throws RipPandaException on failure
+     */
+    private String decollideFilename(Path dir, String filename, boolean unique) throws RipPandaException {
+        String baseName = FilenameUtils.getBaseName(filename);
+        String dottedExtension = filename.substring(baseName.length());
+        String nonCollidingFilename = filename;
 
         int maxSuffix = unique ? 1 : 99;
         for (int i = 1; i <= maxSuffix; ++i) {
@@ -249,14 +372,10 @@ abstract class AbstractElementArchivalService {
                 }
             }
 
-            if (!nonCollidingFilename.equals(sanitizedFilename)) {
-                sanitizedFilename = nonCollidingFilename;
-                LOGGER.debug("Non-colliding filename was adjusted to \"{}\".", sanitizedFilename);
-            }
-            break;
+            return nonCollidingFilename;
         }
 
-        return sanitizedFilename;
+        throw new RuntimeException("Unexpected exception while looking for non-colliding filename.");
     }
 
     /**
